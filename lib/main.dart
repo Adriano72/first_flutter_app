8485681c6ca7912b7f6bf63f19e9e9a0a41d824b
Adriano Massi <adrianomassi@gmail.com>
2019-03-21 16:22:16 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:first_flutter_app/ui/welcome_home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Welcome App",
+    home: Welcome(),
+  ));
+}
+
+
